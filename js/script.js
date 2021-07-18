ffe3c46a97ce7b769b4e57a18f1ee6f5250cbcb1
ARTHUR FINCHAM <arthurfincham@me.com>
2021-07-18 11:51:08 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -17,14 +17,3 @@ $(document).ready(function(){
 
   });
 });
-
-window.transitionToPage = function(href) {
-  document.querySelector('body').style.opacity = 0
-  setTimeout(function() { 
-      window.location.href = href
-  }, 500)
-}
-
-document.addEventListener('DOMContentLoaded', function(event) {
-  document.querySelector('body').style.opacity = 1
-})
